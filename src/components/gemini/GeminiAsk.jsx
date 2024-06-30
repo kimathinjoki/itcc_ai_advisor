dddc7efa136619cedcb048f36b5d0d2986c4518f
kimathinjoki <dfkimathinjoki.com>
2024-06-29 23:12:06 -0500

    add files

---
+++ src/components/gemini/GeminiAsk.jsx
@@ -0,0 +1,10 @@
+
+
+function GeminiAsk(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default GeminiAsk;
---
+++ src/components/openAi/OpenAiAsk.jsx
@@ -0,0 +1,11 @@
+
+
+
+function OpenAiAsk(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default OpenAiAsk;
