d9e941988d5862cde3a0b45b1a4794afb57eb3b1
kimathinjoki <dfkimathinjoki.com>
2024-07-03 12:33:46 -0500

    Remove comment "component" in Navbar component.

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(){
 
     return(
         <>
-        {/* component */}
+        
         <nav className="bg-whiteshadow shadow-gray-300 w-100 px-8 md:px-auto">
             <div className="md:h-16 h-28 mx-auto md:px-4 container flex items-center justify-between flex-wrap md:flex-nowrap">
             {/* Logo */}
